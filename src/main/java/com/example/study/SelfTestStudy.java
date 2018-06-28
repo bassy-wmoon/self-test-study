282e08895c5a5bdbe47500756248bd5da227a8c1
tom bassy <double.moon84@gmail.com>
2018-06-28 10:46:49 +0900

    Write javadoc

--- src/main/java/com/example/study/SelfTestStudy.java
+++ src/main/java/com/example/study/SelfTestStudy.java
@@ -2,6 +2,15 @@ package com.example.study;
 
 public class SelfTestStudy {
 
+    /**
+     * 引数の数値を元に以下のルールに従って変換した文字列を返す
+     * <li>3の倍数の時は Fizz</li>
+     * <li>5の倍数の時は Buzz</li>
+     * <li>3と5の倍数の時は FizzBuzz</li>
+     * <li>それ以外の時は数値</li>
+     * @param number 数値
+     * @return
+     */
     public String fizzBuzz(int number) {
         if (isFizzBuzz(number)) return "FizzBuzz";
         if (isFizz(number)) return "Fizz";
